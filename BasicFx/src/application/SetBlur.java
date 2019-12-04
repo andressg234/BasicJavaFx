d5af7137cd27eff45749a40d13acc8aafddbc54f
andre <andre@192.168.1.46>
2019-12-04 11:36:06 +0100

    BasicJavaFx

---
+++ BasicFx/src/application/SetBlur.java
@@ -0,0 +1,20 @@
+
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.image.ImageView;
+
+//la clase controladora que añade un efecto a la imagen
+public class SetBlur {
+
+	// la etiqueta FXML permite acceder al componente imageView ya creado en
+	// fxml
+	@FXML
+	private ImageView forestview;
+
+	@FXML
+	public void initialize() {
+		
+	}
+}
